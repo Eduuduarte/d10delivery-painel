48d58fd48daba082b55ea35bc9a90881358d636c
Eduardo Duarte <eduardoievolucao@gmail.com>
2023-05-07 10:31:20 -0300

    update

--- src/app/(inside)/pedidos/page.tsx
+++ src/app/(inside)/pedidos/page.tsx
@@ -2,6 +2,7 @@
 
 import { OrderItem } from "@/components/OrderItem";
 import { api } from "@/libs/api";
+import { dateFormat } from "@/libs/dateFormat";
 import { Order } from "@/types/Order";
 import { OrderStatus } from "@/types/OrderStatus";
 import { Refresh, Search } from "@mui/icons-material";
@@ -13,6 +14,7 @@ const Page = () => {
     const [loading, setLoading] = useState(false);
     const [orders, setOrders] = useState<Order[]>([]);
     const [filteredOrders, setFilteredOrders] = useState<Order[]>([])
+    const [printOrder, setPrintOrder] = useState<Order | null>(null)
 
     const getOrders = async () => {
         setSearchInput('');
@@ -37,11 +39,11 @@ const Page = () => {
 
         // Verifica se o usuário clicou em enter
         if (event.code.toLowerCase() === 'enter' || event.code.toLowerCase() === 'numpadenter') {
-            if(searchgInput != '') {
+            if (searchgInput != '') {
                 let newOrders: Order[] = []
 
-                for( let i in orders ) {
-                    if(orders[i].id.toString() === searchgInput) {
+                for (let i in orders) {
+                    if (orders[i].id.toString() === searchgInput) {
                         newOrders.push(orders[i]);
                     }
                 }
@@ -58,71 +60,116 @@ const Page = () => {
         getOrders();
     }
 
-    return (
-        <Box sx={{ my: 3 }}>
+    const handlePrintAction = (order: Order) => {
+        setPrintOrder(order);
+        setTimeout(() => {
+            if (window) window.print();
+        }, 200)
+    }
 
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Box sx={{ mb: 3, display: 'flex', alignItems: 'center' }}>
-                    <Typography component="h5" variant="h5" sx={{ color: "#555", mr: 2 }}>Pedidos</Typography>
-                    {loading && <CircularProgress />}
-                    {!loading &&
-                        <Button onClick={getOrders} size="small" sx={{ justifyContent: { xs: 'flex-start', md: 'center' } }}>
-                            <Refresh />
-                            <Typography
-                                component="div" sx={{ color: "#555", display: { xs: 'none', sm: 'block' } }}
-                            >Atualizar</Typography>
-                        </Button>
+    return (
+        <>
+
+            <Box sx={{ my: 3, displayPrint: 'none' }}>
+
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Box sx={{ mb: 3, display: 'flex', alignItems: 'center' }}>
+                        <Typography component="h5" variant="h5" sx={{ color: "#555", mr: 2 }}>Pedidos</Typography>
+                        {loading && <CircularProgress />}
+                        {!loading &&
+                            <Button onClick={getOrders} size="small" sx={{ justifyContent: { xs: 'flex-start', md: 'center' } }}>
+                                <Refresh />
+                                <Typography
+                                    component="div" sx={{ color: "#555", display: { xs: 'none', sm: 'block' } }}
+                                >Atualizar</Typography>
+                            </Button>
+                        }
+
+                    </Box>
+                    <TextField
+                        value={searchgInput}
+                        onChange={e => setSearchInput(e.target.value)}
+                        onKeyUp={handleSearchKey}
+                        placeholder="Pesquise um pedido"
+                        variant="standard"
+                        disabled={loading}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <Search />
+                                </InputAdornment>
+                            )
+                        }}
+                    />
+                </Box>
+
+                <Grid container spacing={3} columns={{ xs: 1, sm: 2, md: 4 }}>
+                    {loading &&
+                        <>
+                            <Grid item xs={1}>
+                                <Skeleton variant="rectangular" height={220} />
+                            </Grid>
+                            <Grid item xs={1}>
+                                <Skeleton variant="rectangular" height={220} />
+                            </Grid>
+                            <Grid item xs={1}>
+                                <Skeleton variant="rectangular" height={220} />
+                            </Grid>
+                            <Grid item xs={1}>
+                                <Skeleton variant="rectangular" height={220} />
+                            </Grid>
+                            <Grid item xs={1}>
+                                <Skeleton variant="rectangular" height={220} />
+                            </Grid>
+                        </>
                     }
 
-                </Box>
-                <TextField
-                    value={searchgInput}
-                    onChange={e => setSearchInput(e.target.value)}
-                    onKeyUp={handleSearchKey}
-                    placeholder="Pesquise um pedido"
-                    variant="standard"
-                    disabled={loading}
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <Search />
-                            </InputAdornment>
-                        )
-                    }}
-                />
+                    {!loading && filteredOrders.map((item, index) => (
+                        <Grid key={index} item xs={1}>
+                            <OrderItem
+                                item={item}
+                                onChangeStatus={handleChangeStatus}
+                                onPrint={handlePrintAction}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
             </Box>
-
-            <Grid container spacing={3} columns={{ xs: 1, sm: 2, md: 4 }}>
-                {loading &&
+            <Box sx={{ display: 'none', displayPrint: 'block' }}>
+                {printOrder &&
                     <>
-                        <Grid item xs={1}>
-                            <Skeleton variant="rectangular" height={220} />
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Skeleton variant="rectangular" height={220} />
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Skeleton variant="rectangular" height={220} />
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Skeleton variant="rectangular" height={220} />
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Skeleton variant="rectangular" height={220} />
-                        </Grid>
+                        <Typography component="h5" variant="h5">Pedido</Typography>
+                        <Box>ID: #{printOrder.id}</Box>
+                        <Box>Data do Pedido: {dateFormat(printOrder.orderDate)}</Box>
+                        <Box>Cliente: {printOrder.userName}</Box>
+
+                        <Typography component="h5" variant="h5">Pagamento</Typography>
+                        <Box>Tipo de pagamento: {printOrder.paymentType === 'card' ? "Cartão" : "Dinheiro"}</Box>
+                        <Box>Subtotal: R$ {printOrder.subtotal.toFixed(2)}</Box>
+                        <Box>Entrega: R$ {printOrder.shippingPrice.toFixed(2)}</Box>
+                        {printOrder.cupomDiscount && <Box>Desconto: -R$ {printOrder.cupomDiscount.toFixed(2)}</Box>}
+                        <Box>Total: R$ {printOrder.total.toFixed(2)}</Box>
+
+                        <Typography component="h5" variant="h5">Endereço</Typography>
+                        <Box>Rua: {printOrder.shippingAddress.address}</Box>
+                        <Box>Número: {printOrder.shippingAddress.number}</Box>
+                        <Box>Complemento: {printOrder.shippingAddress.complement}</Box>
+                        <Box>Cep: {printOrder.shippingAddress.cep}</Box>
+                        <Box>Bairro: {printOrder.shippingAddress.neighborhood}</Box>
+                        <Box>Cidade: {printOrder.shippingAddress.city}</Box>
+                        <Box>Estado: {printOrder.shippingAddress.state}</Box>
+
+                        <Typography component="h5" variant="h5">Itens</Typography>
+                        {printOrder.products.map((item, index) => (
+                            <Box key={index}>
+                                {item.qt}x {item.product.name}
+                            </Box>
+                        ))}
                     </>
                 }
+            </Box>
+        </>
 
-                {!loading && filteredOrders.map((item, index) => (
-                    <Grid key={index} item xs={1}>
-                        <OrderItem
-                            item={item}
-                            onChangeStatus={handleChangeStatus}
-                        />
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
     )
 }
 
